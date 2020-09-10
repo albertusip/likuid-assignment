3d4a6aded07aaf6447f3deb17e6e3da0a6b5de6f
Albertus Istora Pahala <albertus.istora@ti.ukdw.ac.id>
2020-09-10 22:52:23 +0700

    Refactor code

--- src/components/SearchForm/index.js
+++ src/components/SearchForm/index.js
@@ -1,9 +1,7 @@
 import React, { memo, useState } from 'react'
 
 import Form from 'react-bootstrap/Form';
-import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import FormControl from 'react-bootstrap/FormControl';
 import Button from 'react-bootstrap/Button';
 
 const SearchForm = ({onSubmit}) => {
@@ -21,23 +19,23 @@ const SearchForm = ({onSubmit}) => {
     }
 
     return (
-        <Row md={3} className="align-items-center d-flex justify-content-center">
-            <Col sm={12} className="my-1">
+        <>
+            <Col sm={12} md={4} className="my-1">
                 <Form.Label htmlFor="formSearchUniversity" srOnly>
                     Search University
                 </Form.Label>
                 <Form.Control value={search.universityName} onChange={handleChange('universityName')} id="formSearchUniversity" placeholder="University Name" />
             </Col>
-            <Col sm={12} className="my-1">
+            <Col sm={12} md={4} className="my-1">
                 <Form.Label htmlFor="formSearchCountry" srOnly>
                     Search Country
                 </Form.Label>
-                <FormControl value={search.universityCountry} onChange={handleChange('universityCountry')} id="formSearchCountry" placeholder="University Country" />
+                <Form.Control value={search.universityCountry} onChange={handleChange('universityCountry')} id="formSearchCountry" placeholder="University Country" />
             </Col>
             <Col sm={12} md={2} className="my-1">
                 <Button onClick={handleSubmit} type="submit" className="w-100 btn-blue--light">Search</Button>
             </Col>
-        </Row>
+        </>
     );
 }
 
