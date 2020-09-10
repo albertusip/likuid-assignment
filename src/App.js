84bff2b626f81628764fbc707c51daae46341a23
Albertus Istora Pahala <albertus.istora@ti.ukdw.ac.id>
2020-09-10 22:51:51 +0700

    Added newsletter component in App.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import React, { useState } from 'react'
 
 import SearchForm from './components/SearchForm'
 import UniversityCards from './components/UniversityCards'
+import NewsLetter from './components/NewsLetter'
 
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
 const App = () => {
+    const [modalNewsLetter, setModalNewsLetter] = useState({
+        show: false
+    });
     const [formValue, setFormValue] = useState();
     const handleFormSubmit = (value) => {
         setFormValue(value);
@@ -25,27 +30,32 @@ const App = () => {
                 <Navbar.Brand href="/">University Finder</Navbar.Brand>
             </Navbar>
             <Container fluid>
-                <Row>
-                    <Col
-                        sm={12}
-                        className="mt-5 mb-3"    
-                    >
-                        <Container>
+                <Row className="align-items-center d-flex justify-content-between mt-5 mb-3">
+                    <Col sm={12} md={9} lg={10}>
+                        <Row>
                             <SearchForm
                                 onSubmit={handleFormSubmit}
                             />
-                        </Container>
+                        </Row>
                     </Col>
-                    <Col sm={12}>
-                        <Card>
-                            <Card.Body>
-                                <UniversityCards
-                                    formValue={formValue}
-                                />
-                            </Card.Body>
-                        </Card>
+                    <Col sm={12} md={3} lg={2} className="text-right">
+                        <Button onClick={() => setModalNewsLetter({ show: true })} className="btn-green--light w-100">
+                            News Letter
+                        </Button>
+                        <NewsLetter
+                            formValue={modalNewsLetter}
+                            show={modalNewsLetter.show}
+                            onHide={() => setModalNewsLetter({show: false})}
+                        />
                     </Col>
                 </Row>
+                <Card>
+                    <Card.Body>
+                        <UniversityCards
+                            formValue={formValue}
+                        />
+                    </Card.Body>
+                </Card>
             </Container>
         </>
     );
